d742a5aa51ce80e457119fc0fec07983bdbd1cff
marcusvolz <marcus.volz@outlook.com>
2018-04-30 19:48:02 +1000

    added triangulate and tessellate functions

---
+++ R/tessellate.R
@@ -0,0 +1,13 @@
+#' Create a tessellatation for a given set of points
+#'
+#' Creates a tessellatation for a given set of points
+#' @param points A data frame with point x and y coordinates
+#' @return A data frame with segment x, y, xend and yend coorindates
+#' @details A simple wrapper for deldir::deldir()
+#' @export
+#' @examples data.frame(x = runif(10), y = runif(10)) %>% tessellate()
+
+tessellate <- function(points) {
+  deldir(points)$dirsgs %>%
+    rename(x = x1, y = y1, xend = x2, yend = y2)
+}
---
+++ R/triangulate.R
@@ -0,0 +1,13 @@
+#' Create a triangulation for a given set of points
+#'
+#' Creates a triangulation for a given set of points
+#' @param points A data frame with point x and y coordinates
+#' @return A data frame with segment x, y, xend and yend coorindates
+#' @details A simple wrapper for deldir::deldir()
+#' @export
+#' @examples data.frame(x = runif(10), y = runif(10)) %>% triangulate()
+
+triangulate <- function(points) {
+  deldir(points)$delsgs %>%
+    rename(x = x1, y = y1, xend = x2, yend = y2)
+}
