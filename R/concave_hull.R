4749014a9de4793e670a426d92cd0ae8d6f2cc97
Marcus Gordon Volz <marcus.volz@outlook.com>
2018-05-08 15:00:39 +1000

    added function for computing the concave hull for a given set of points

---
+++ R/concave_hull.R
@@ -0,0 +1,17 @@
+#' Concave hull
+#'
+#' Computes the concave hull for a set of points (wrapper for concaveman::concaveman)
+#' @param points A data frame of points.
+#' @param concavity A value that determines the concavity (1 = detailed shape, Inf = convex hull).
+#' @param length_threshold Segment lengths less than this threshold are not considered; higher values give simpler shapes.
+#' @keywords concave hull
+#' @export
+#' @examples
+#' concave_hull()
+
+concave_hull <- function(points, concavity = 1, length_threshold = 0) {
+  points <- as.matrix(points)
+  result <- concaveman(points, concavity, length_threshold) %>%
+    as.data.frame() %>%
+    rename(x = V1, y = V2)
+}
